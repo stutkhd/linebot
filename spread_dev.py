e3474e00ca1e0bc9870b933e0f6cc0f07c72c4d3
佐藤鷹英 <satotakahide@satoutakaeinoMacBook-Air.local>
2020-11-14 00:11:35 +0900

    [A ] add detect last low func

--- spread_dev.py
+++ spread_dev.py
@@ -17,20 +17,24 @@ title = f'{month}/{day}'
 
 #keyでworkbook取得 -> keyじゃないとコピーとかできないらしい
 wb = gc.open_by_key(sheet_key)
-#シートの一覧
 worksheet_list = list(map(lambda x:x.title, wb.worksheets()))
 
-# #1番左は何も書いていないフォーマットにする
-form = wb.get_worksheet(0) #コピーするsheet
+form = wb.get_worksheet(0)
 if len(worksheet_list) > 1:
     ws1 = wb.get_worksheet(1) #一番最新の検温記録
     ws1_title = ws1.title
 
-#すでにファイルが存在する
-if title in worksheet_list:
-    #userが入力を行う
-    
-else:
-    #ファイルをコピーする
+#正しく機能させるために1,2,3が抜けていた場合はエラーを吐くように入力フォームを注意する必要あり
+def detect_last_row(worksheet):
+    row_count = 5
+    while worksheet.cell(row_count,2).value != "" or worksheet.cell(row_count,3).value != "" or worksheet.cell(row_count,4).value != "":
+        row_count += 1
+    return row_count
+
+if title not in worksheet_list:
+    #新規sheet作成
     wb.duplicate_sheet(source_sheet_id = form.id, new_sheet_name = title, insert_sheet_index=1)
 
+now = ws.get_worksheet(2)
+#最後の行を取得
+last_row = detect_last_row(now)
